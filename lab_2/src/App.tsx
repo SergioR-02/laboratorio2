eb34144793ef7ba56b179f1984b741dc5d1fcdea
SergioR-02 <seruizh@unal.edu.co>
2024-07-14 00:50:22 -0500

    Title

--- lab_2/src/App.tsx
+++ lab_2/src/App.tsx
@@ -1,11 +1,18 @@
 import Form from './components/formulario/Form'
+import AddForm from './components/addForm/AddForm'
+import { useState } from 'react'
+
 
 function App() {
+  const [add, setAdd] = useState(false)
 
-  
+  const handleChange = () => {
+    setAdd(!add)
+  }
   return (
     <>
-      <Form />
+      <AddForm handleChange={handleChange} />
+      {add && <Form />}
     </>
   )
 }
---
+++ lab_2/src/components/addForm/AddForm.tsx
@@ -0,0 +1,16 @@
+import "./AddForm.css";
+
+interface AddFormProps {
+  handleChange: () => void;
+}
+function AddForm({handleChange}: AddFormProps) {
+  
+  return(
+    <section className="titulo" >
+      <h2>Mis Tareas</h2>
+      <img onClick={handleChange} src="/img/add.png" alt="Logo de la organizacion"/>
+    </section>
+  )
+}
+
+export default AddForm;
