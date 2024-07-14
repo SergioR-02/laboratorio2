380e5f212a93a581388f6e5af634e95e1db0a37e
SergioR-02 <seruizh@unal.edu.co>
2024-07-13 22:02:57 -0500

    Delete

--- dist/tareas.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-class Tareas {
-    constructor(id, descripcion, estado, prioridad) {
-        this.id = id;
-        this.descripcion = descripcion;
-        this.estado = estado;
-        this.prioridad = prioridad;
-    }
-}
